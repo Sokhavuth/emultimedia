77ad24c1c8ea081ee10a66c701ec084b7f9c6d26
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-02-03 07:23:29 +0700

    Make it better

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+global.__basedir = __dirname;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,7 +19,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'react/frontend/build')));
+app.use(express.static(path.join(__dirname, 'react/backend/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(__basedir+'/react/frontend/build/index.html');
 });
 
 router.get('/api', function(req, res, next){
